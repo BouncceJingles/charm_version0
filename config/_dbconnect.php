3bf92e38911684814c99deb1ecd62120ec00d99e
Hong Jing <lonedune@gmail.com>
2013-12-06 17:42:01 +0800

    removed the olds and the backups

--- config/_dbconnect.php
+++ config/_dbconnect.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'charm');
+define('DB_SERVER', 'mysql6.000webhost.com');
+define('DB_USERNAME', 'a9810835_charm');
+define('DB_PASSWORD', '`12345q');
+define('DB_DATABASE', 'a9810835_charm');
 
 mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD)or die(mysql_error()); 
 mysql_select_db(DB_DATABASE)or die(mysql_error());
